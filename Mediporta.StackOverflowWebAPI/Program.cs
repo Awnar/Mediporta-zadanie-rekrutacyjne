d6de6f1bac0d4bb5a4ef3e80df6666d75e7e1d49
PC <PC@DESKTOP-877GQ5Q>
2024-04-02 20:48:17 +0200

    adding ErrorHandlingMiddleware and logs

--- Mediporta.StackOverflowWebAPI/Program.cs
+++ Mediporta.StackOverflowWebAPI/Program.cs
@@ -3,6 +3,10 @@ using Mediporta.StackOverflowWebAPI.Repositories;
 using Mediporta.StackOverflowWebAPI.Repositories.Interfaces;
 using Mediporta.StackOverflowWebAPI.Services;
 using Mediporta.StackOverflowWebAPI.Services.Interface;
+using NLog;
+using NLog.Extensions.Logging;
+using NLog.Web;
+using StackOverflow.Tags.Middleware;
 using System.Text.Json.Serialization;
 
 namespace Mediporta.StackOverflowWebAPI
@@ -11,37 +15,54 @@ namespace Mediporta.StackOverflowWebAPI
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddScoped<ITagsService, TagsService>();
-            builder.Services.AddScoped<ICacheRepository, CacheRepository>();
-            builder.Services.AddStackExchangeRedisCache(options => { options.Configuration = builder.Configuration.GetConnectionString("RedisConnection"); });
-            builder.Services.AddAutoMapper(typeof(Program));
-
-            builder.Services.AddControllers()
-                .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                var builder = WebApplication.CreateBuilder(args);
+
+                // Add services to the container.
+                builder.Services.AddScoped<ITagsService, TagsService>();
+                builder.Services.AddScoped<ICacheRepository, CacheRepository>();
+                builder.Services.AddStackExchangeRedisCache(options => { options.Configuration = builder.Configuration.GetConnectionString("RedisConnection"); });
+                builder.Services.AddAutoMapper(typeof(Program));
+                builder.Services.AddScoped<ErrorHandlingMiddleware>();
+
+                builder.Services.AddControllers()
+                    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                builder.Services.AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.AddNLog();
+                });
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseMiddleware<ErrorHandlingMiddleware>();
+                app.UseAuthorization();
+                app.MapControllers();
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            catch (Exception exception)
+            {
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
     }
 }
